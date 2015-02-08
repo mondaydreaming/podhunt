8867243df9f606aec250f2b5683da7eda40d8867
Bishin Ho <bishin.ho@gmail.com>
2015-02-08 16:55:25 +1100

    hating this

--- db/migrate/20150206232104_create_podcasts.rb
+++ db/migrate/20150206232104_create_podcasts.rb
@@ -1,12 +1,11 @@
 class CreatePodcasts < ActiveRecord::Migration
   def change
     create_table :podcasts do |t|
-        t.string :name
-        t.text :podcaster
-        t.text :blurb
-        t.text :image
-        t.text :genres
+        t.string :title
+        t.text :feedURL
+        t.text :author
+        t.text :description
+        t.text :categories
         t.timestamps
     end
-  end
 end

55af11aad6e4288bbe00a42608059a87a3d16cdc
Bishin Ho <bishin.ho@gmail.com>
2015-02-10 10:58:51 +1100

    adding bootstrap sass js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13,4 +15,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+//= require bootstrap-sprockets
+
 //= require_tree .

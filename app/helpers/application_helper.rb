a5021ab4716a58ed6a524e82cacdbb727ff80ac4
Bishin Ho <bishinho@Bishins-MacBook-Air.local>
2015-02-10 16:52:50 +1100

    just need to do search ui styling favouriting and hash tagging now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
     def nav_menu
-        links = "<li>#{link_to("Home", root_path)}</li><li>#{link_to("Search", search_podcasts_path)}</li>"
+        links = "<li>#{link_to("Home", root_path)}</li>
+        <li>#{link_to("Search for new Podcast", search_podcasts_path)}</li>
+        <li>#{link_to("Browse other listeners", listeners_path)}</li>"
         if @current_listener.present?
             links += "<li>#{link_to("Log out", login_path, :method => :delete)} </li>
             <li>#{link_to("Edit account", edit_listener_path(@current_listener.id))}</li>
--- app/models/message.rb
+++ app/models/message.rb
@@ -12,7 +12,7 @@
 #
 
 class Message < ActiveRecord::Base
-    belongs_to :episode
+    belongs_to :podcast
     belongs_to :listener
     validates :comment, :presence => true
 end

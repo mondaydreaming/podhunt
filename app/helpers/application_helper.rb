a445fb670dc395e6ee581ab994c4c18e7f4dce5c
Bishin Ho <bishinho@Bishins-MacBook-Air.local>
2015-02-11 22:28:22 +1100

    some styling is working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,20 @@
 module ApplicationHelper
-    def nav_menu
-        links = "<li>#{link_to("Home", root_path)}</li>
-        <li>#{link_to("Search for new Podcast", search_podcasts_path)}</li>
-        <li>#{link_to("Browse other listeners", listeners_path)}</li>"
+    def my_account_nav
         if @current_listener.present?
-            links += "<li>#{link_to("Log out", login_path, :method => :delete)} </li>
-            <li>#{link_to("Edit account", edit_listener_path(@current_listener.id))}</li>
-            <li>#{link_to("My playlist", podcasts_path)}</li>"
+            links = "
+                  <li>#{link_to("Log out", login_path, :method => :delete)}</li>
+                  <li>#{link_to("Edit account", edit_listener_path(@current_listener.id))}</li>
+                  <li>#{link_to("My playlist", podcasts_path)}</li>"
         else
-            links += "<li>#{link_to("Sign up", new_listener_path)}</li><li>#{link_to("Log in", login_path)} </li>"
+            links = "
+                  <li>#{link_to("Sign up", new_listener_path)}</li>
+                  <li>#{link_to("Log in", login_path)}</li>"
         end
-        links
+            links
     end
 end
+
+
+
+
+

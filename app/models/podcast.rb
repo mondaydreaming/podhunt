9911ddebcde3782d0dcec924786266ea52273ebf
Bishin Ho <bishinho@Bishins-MacBook-Air.local>
2015-02-13 10:32:26 +1100

    debugging

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,18 +2,21 @@
 #
 # Table name: podcasts
 #
-#  id            :integer          not null, primary key
-#  title         :string
-#  feed_url      :text
-#  author        :text
-#  description   :text
-#  categories    :text
-#  image         :text
-#  last_modified :text
-#  url           :text
-#  entries       :text
-#  created_at    :datetime
-#  updated_at    :datetime
+#  id          :integer          not null, primary key
+#  title       :string
+#  feed_url    :text
+#  author      :text
+#  description :text
+#  categories  :text
+#  image       :text
+#  url         :text
+#  entries     :text
+#  copyright   :text
+#  subtitle    :text
+#  language    :text
+#  explicit    :text
+#  created_at  :datetime
+#  updated_at  :datetime
 #
 
 class Podcast < ActiveRecord::Base

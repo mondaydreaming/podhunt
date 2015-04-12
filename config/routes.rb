e75d9df837dc905bded88a891d9fad9fff11195d
Bishin Ho <bishin.ho@gmail.com>
2015-04-12 20:48:16 +1000

    tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
     resources :messages #, :except => :create
   end
 
-  # post '/podcasts/:podcast_id/messages' => 'messages#create', :as => "create_message"
 
   get '/search/podcasts' => 'podcasts#search', :as => "search_podcasts"
   post '/podcasts/:id/subscribe' => 'podcasts#subscribe', :as => "podcast_subscribe"
